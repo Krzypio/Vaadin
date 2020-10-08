80a555fde86aca473a2117fbfe28d4ca19406ecd
KrzysztofPio <krzysztofp10git@op.pl>
2020-10-08 21:03:48 +0200

    Create step by step
    Opis krok po kroku jak utworzyć nowy projekt z odpowiednimi zależnościami.

---
+++ stany/src/main/java/praca/stany/StanyApplication.java
@@ -0,0 +1,13 @@
+package praca.stany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StanyApplication.class, args);
+	}
+
+}
---
+++ stany/src/test/java/praca/stany/StanyApplicationTests.java
@@ -0,0 +1,13 @@
+package praca.stany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
